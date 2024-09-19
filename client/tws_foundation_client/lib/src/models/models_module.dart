76a36f482bc8e5ded5c0f689477398b7a327f862
Juan Renato Urrea Ortiz <UsuaryRenato@hotmail.com>
2024-09-19 13:36:22 -0700

    exposition

--- client/tws_foundation_client/lib/src/models/models_module.dart
+++ client/tws_foundation_client/lib/src/models/models_module.dart
@@ -1,9 +1,8 @@
-// ignore_for_file: directives_ordering
-
-export 'out/models_out_moudle.dart';
-export 'options/models_options_module.dart';
 
 export 'credentials.dart';
+export 'interfaces/models_interfaces_module.dart';
+export 'options/models_options_module.dart';
+export 'out/models_out_moudle.dart';
 export 'privileges.dart';
 export 'server_exception.dart';
 export 'source_transaction_failure.dart';
