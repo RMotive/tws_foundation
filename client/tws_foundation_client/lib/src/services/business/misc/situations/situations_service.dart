5c5ff563467f631bc6907a560b288e4a61acd663
jr110 <c.d@hotmail.es>
2025-07-22 00:48:17 -0700

    situation interfaces implementations

--- client/tws_foundation_client/lib/src/services/business/misc/situations/situations_service.dart
+++ client/tws_foundation_client/lib/src/services/business/misc/situations/situations_service.dart
@@ -4,17 +4,13 @@ import 'package:tws_foundation_client/tws_foundation_client.dart';
 /// {interface} for [SituationsServiceI].
 ///
 /// Defines base contract for [SituationsServiceI] implementations that specifies the methods to have providing [Situation] based operations and management.
-abstract interface class SituationsServiceI extends FoundationServiceB implements ServiceI {
+abstract interface class SituationsServiceI extends FoundationServiceB implements ServiceI, ViewServiceI<Situation> {
   /// Creates a new [SituationsServiceI] instance.
-  SituationsServiceI(super.host, super.servicePath);
+  SituationsServiceI(
+    super.host,
+    super.servicePath,
+  );
 
-  /// Generates a complex [View] for [Situation] set.
-  ///
-  ///
-  /// [options] how the method will build the [View] result, are instructions for the paging, ordering, etc.
-  ///
-  /// [auth] server authorization token.
-  FoundationFutureResolver<ViewOutput<Situation>> view(ViewInput<Situation> input, String auth);
 }
 
 /// {abstract} class for [SituationsServiceB].
